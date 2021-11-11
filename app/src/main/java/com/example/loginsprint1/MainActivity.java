9207f21ee8f873223d1a926d26413a3b8c96b663
Grupo4Ciclo3 <89669906+Grupo4Ciclo3@users.noreply.github.com>
2021-11-10 20:41:20 -0500

    Entrega Sprint 1

--- app/src/main/java/com/example/loginsprint1/MainActivity.java
+++ app/src/main/java/com/example/loginsprint1/MainActivity.java
@@ -1,13 +1,35 @@
 package com.example.loginsprint1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }
+
+    //Metodo para el boton ingreso
+
+    public void Ingres(View view){
+
+        Intent ingres = new Intent (this, IngresActivity.class);
+        startActivity(ingres);
+
+    }
+    //Metodo para el boton registro
+
+    public void Register(View view){
+        Intent register = new Intent (this, RegisterActivity.class);
+        startActivity(register);
     }
 }
